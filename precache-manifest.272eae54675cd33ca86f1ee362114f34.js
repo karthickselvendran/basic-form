c917a441065369ba2f78eb87742a10b68a9783de
karthickselvendran <selvendrankarthick@gmail.com>
2020-06-01 12:36:05 +0530

    Updates

---
+++ precache-manifest.272eae54675cd33ca86f1ee362114f34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ea5536283e1e1079bcb937885279489",
+    "url": "/basic-form/index.html"
+  },
+  {
+    "revision": "6c27295911b194b1e1f6",
+    "url": "/basic-form/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "87e6cad170e2721c5c61",
+    "url": "/basic-form/static/js/2.8ec6e315.chunk.js"
+  },
+  {
+    "revision": "e2fc3609d47d90b8cc004bfe15ddf15e",
+    "url": "/basic-form/static/js/2.8ec6e315.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6c27295911b194b1e1f6",
+    "url": "/basic-form/static/js/main.91996c2a.chunk.js"
+  },
+  {
+    "revision": "6d00ab8c0f166ab7ac03",
+    "url": "/basic-form/static/js/runtime-main.41f1c9f3.js"
+  }
+]);
